badca9a3c5a1d9eabc30f68eb28c19d4b098fdcd
Julia-Moirano <128604705+Julia-Moirano@users.noreply.github.com>
2024-05-29 16:10:22 -0300

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,10 +1,12 @@
 let c = [];
+let c2 = [];
 let cantidad = [...Array(10).keys()];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cantidad.forEach((element) => {
-    c.push(new Caminante(windowWidth / 2, random(0, windowHeight)));
+    c.push(new Caminante(0, random(0, windowHeight)));
+    c2.push(new Caminante(random(0, windowWidth), 0));
   });
 
   background("#222222");
@@ -13,15 +15,19 @@ function setup() {
 
 function draw() {
   //this.rectangulo.dibujar(100,100);
-  // push();
-  // noStroke();
-  // fill(0, 0, 10, 5);
-  // rect(0, 0, width, height);
-  // pop();
+   push();
+   noStroke();
+   fill(0, 0, 10, 5);
+   rect(0, 0, width, height);
+   pop();
 
   c.forEach((caminante) => {
     caminante.mover(9);
-    // caminante.actualizar(9);
+    caminante.dibujar();
+  });
+  
+  c2.forEach((caminante) => {
+    caminante.mover2(9);
     caminante.dibujar();
   });
 }
