e061bb9a90d718398bc37aeb644fe3d011c0a954
Julia-Moirano <128604705+Julia-Moirano@users.noreply.github.com>
2024-05-29 16:11:44 -0300

    Update claseCaminante.js

--- scripts/claseCaminante.js
+++ scripts/claseCaminante.js
@@ -19,9 +19,31 @@ class Caminante {
     image(this.pincelada, 0, 0, 30, 30);
     pop();
   }
-
-  mover() {
+  
+  mover(vel) {
     this.dir += random(-15, 15);
+    this.x += vel;
+     if(this.x > width){
+        this.x = 0;
+        this.y = random(0,windowHeight);
+   }
+      if (frameCount > 0){ 
+        this.y = random(this.y - 2, this.y + 2);
+   }
+    this.x += this.vel * Math.cos(toRad(this.dir));
+    this.y += this.vel * Math.sin(toRad(this.dir));
+  }
+  
+  mover2(vel) {
+    this.dir += random(-15, 15);
+    this.y += vel;
+    if(this.y > height){
+      this.y = 0;
+      this.x = random(windowWidth,0);
+ }
+    if (frameCount > 0){ 
+      this.x = random(this.x - 2, this.x + 2);
+ }
     this.x += this.vel * Math.cos(toRad(this.dir));
     this.y += this.vel * Math.sin(toRad(this.dir));
   }
