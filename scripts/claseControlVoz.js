b7a008fdfe2433bfb70a93d5e81a8c9ff32c12b5
Marco Marino <marcomarinoj@gmail.com>
2024-05-29 04:55:53 -0300

    mouseIsPressed

---
+++ scripts/claseControlVoz.js
@@ -0,0 +1,7 @@
+class ControlVoz {
+  constructor() {}
+
+  haySonido() {
+    return mouseIsPressed;
+  }
+}
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,5 +1,6 @@
 let c = [];
 let cantidad = [...Array(10).keys()];
+let controlVoz;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -7,12 +8,14 @@ function setup() {
     c.push(new Caminante(windowWidth / 2, random(0, windowHeight)));
   });
 
+  controlVoz = new ControlVoz();
+
   background("#222222");
   fill(0);
 }
 
 function draw() {
-  if (mouseIsPressed) {
+  if (controlVoz.haySonido()) {
     c.forEach((caminante) => {
       let variacion = mouseY;
       variacion = map(variacion, 0, windowWidth, 0.5, 3);
